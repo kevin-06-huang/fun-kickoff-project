7534ca8c266c9fe2a489ebadfb0317d44704cc21
Kevin <kevin.06.huang@gmail.com>
2022-01-23 17:28:34 -0800

    Added Profile, Appointment schema

--- server/models/Appointment.js
+++ server/models/Appointment.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcryptjs");
 
-const userSchema = new mongoose.Schema({
+//TABLE [Appointment]
+//- [Email] String
+//- [StartTime] Date
+//- [EndTime] Date
+//- [IsRecurring] Bool
+const appointmentSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
@@ -10,29 +14,26 @@ const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: false
   },
-  password: {
+  description: {
     type: String,
-    required: true
+    required: false,
+    unique: false
   },
-  register_date: {
+  startTime: {
+
+  },
+  endTime: {
+
+  },
+  isRecurring: {
+
+  },
+  lastModified: {
     type: Date,
     default: Date.now
   }
 });
 
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
-});
-
-module.exports = User = mongoose.model("user", userSchema);
+module.exports = Appointment = mongoose.model("appointment", appointmentSchema);
--- server/models/Profile.js
+++ server/models/Profile.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
-
+//payment?
 const profileSchema = new mongoose.Schema({
   first_name: {
     type: String,
@@ -20,23 +20,17 @@ const profileSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
+  profilePic: {
+    type: Image,
+    default: ''
+  },
+  availability: {
+    type: [{type: Schema.Types.ObjectId, ref:'Appointment'}],
+  },
   register_date: {
     type: Date,
     default: Date.now
   }
-});
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
-
-userSchema.pre("save", async function (next) {
-  if (!this.isModified("password")) {
-    next();
-  }
-
-  const salt = await bcrypt.genSalt(10);
-  this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = Profile = mongoose.model("profile", profileSchema);
