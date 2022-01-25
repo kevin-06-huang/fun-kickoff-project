52424217e5b2fa63e7eea0ad2796a3f87c33452e
Kevin <kevin.06.huang@gmail.com>
2022-01-24 21:22:32 -0800

    Casing

---
+++ client/src/components/Sidebar/SidebarData.tsx
@@ -0,0 +1,26 @@
+export const SidebarData = [
+  {
+    title: 'Edit Profile',
+    component: 'EditProfile',
+  },
+  {
+    title: 'Profile Photo',
+    component: 'ProfilePhoto',
+  },
+  {
+    title: 'Availability',
+    component: 'Availability',
+  },
+  {
+    title: 'Payment',
+    component: 'Payment',
+  },
+  {
+    title: 'Security',
+    component: 'Security',
+  },
+  {
+    title: 'Settings',
+    component: 'Settings',
+  },
+];
