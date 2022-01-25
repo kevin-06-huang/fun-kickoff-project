b38ed84d0479a5f4ac90b0ffa3f3ebd2c5a0437e
Kevin <kevin.06.huang@gmail.com>
2022-01-24 23:11:28 -0800

    Added form to profile edit

--- client/src/components/EditProfile/EditProfile.tsx
+++ client/src/components/EditProfile/EditProfile.tsx
@@ -8,6 +8,14 @@ import { User } from '../../interface/User';
 import Avatar from '@mui/material/Avatar';
 
 import { Formik, FormikHelpers } from 'formik';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+import * as Yup from 'yup';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import FormHelperText from '@mui/material/FormHelperText';
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import CircularProgress from '@mui/material/CircularProgress';
 
 /*
   first name, last name, gender, birth date, email address, phone number,where you live, describe yourself
@@ -48,6 +56,42 @@ const EditProfile: React.FC<Props> = ({ loggedInUser, handleSubmit }) => {
       {[...Array(30)].map((id) => (
         <Avatar key={id} alt="Profile Image" src={`https://robohash.org/${makeid()}.png`} />
       ))}
+      <Formik
+        initialValues={{
+          email: '',
+        }}
+        validationSchema={Yup.object().shape({
+          email: Yup.string().required('Email is required').email('Email is not valid'),
+        })}
+        onSubmit={handleSubmit}
+      >
+        {({ handleSubmit, handleChange, values, touched, errors, isSubmitting }) => (
+          <form onSubmit={handleSubmit} className={classes.form} noValidate>
+            <FormControl id="email" fullWidth={true} margin="dense">
+              <FormLabel>
+                <Typography className={classes.label}>EMAIL ADDRESS</Typography>
+              </FormLabel>
+              <OutlinedInput
+                className={classes.inputs}
+                name="email"
+                autoComplete="email"
+                autoFocus={true}
+                error={touched.email && Boolean(errors.email)}
+                value={values.email}
+                placeholder="Your email"
+                onChange={handleChange}
+              />
+              <FormHelperText>{touched.email ? errors.email : ''}</FormHelperText>
+            </FormControl>
+            <Box textAlign="center" marginTop={5}>
+              <Button type="submit" size="large" variant="contained" color="secondary" className={classes.submit}>
+                {isSubmitting ? <CircularProgress style={{ color: 'white' }} /> : 'Save'}
+              </Button>
+            </Box>
+            <Box height={95} />
+          </form>
+        )}
+      </Formik>
     </>
   );
 };
--- client/src/components/EditProfile/useStyles.ts
+++ client/src/components/EditProfile/useStyles.ts
@@ -1,6 +1,7 @@
+import { Theme } from '@mui/material/styles';
 import { makeStyles } from '@mui/styles';
 
-const useStyles = makeStyles(() => ({
+const useStyles = makeStyles((theme: Theme) => ({
   root: {
     minHeight: '100vh',
     '& .MuiInput-underline:before': {
@@ -28,6 +29,32 @@ const useStyles = makeStyles(() => ({
     color: '#000000',
     fontWeight: 700,
     textAlign: 'center',
+  },
+  form: {
+    width: '100%', // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  label: {
+    fontSize: 19,
+    color: 'rgb(0,0,0,1)',
+  },
+  inputs: {
+    height: '3.5rem',
+    padding: '5px',
+  },
+  forgot: {
+    paddingRight: 10,
+    color: '#3a8dff',
+  },
+  submit: {
+    margin: theme.spacing(3, 2, 2),
+    padding: 10,
+    width: 160,
+    height: 56,
+    borderRadius: theme.shape.borderRadius,
+    fontSize: 16,
+    backgroundColor: '#3a8dff',
+    fontWeight: 'bold',
   },
 }));
 
