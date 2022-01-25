9de12ca495c0ef033ff88d9b88eac4245873dc4d
Kevin <kevin.06.huang@gmail.com>
2022-01-24 19:05:30 -0800

    Added controller, route for profile.

--- server/models/User.js
+++ server/models/User.js
@@ -12,6 +12,11 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true
   },
+  id: {
+    type: Number,
+    required: true,
+    unique: true
+  },
   password: {
     type: String,
     required: true
