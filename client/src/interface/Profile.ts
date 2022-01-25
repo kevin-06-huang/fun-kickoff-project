6a21b10aa9e43b2122cb4062c9c47d3f00e4f382
Kevin <kevin.06.huang@gmail.com>
2022-01-24 19:38:33 -0800

    Working on controllers

---
+++ client/src/interface/Profile.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+    firt_name: string;
+    last_name: string;
+    description: string;
+    email: string;
+    profilePic: string;
+    availability: [string];
+    register_date: Date;
+  }
+  
